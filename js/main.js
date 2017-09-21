d57dabb0fe748ecf0c7353923428499c952e8354
Liz T <liz@liztracywriting.com>
2017-09-21 09:37:45 +0400

    countdown timer added to homepage

--- js/main.js
+++ js/main.js
@@ -1,18 +1,4 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<style>
-p {
-  text-align: center;
-  font-size: 60px;
-}
-</style>
-</head>
-<body>
 
-<p id="demo"></p>
-
-<script>
 // Set the date we're counting down to
 var countDownDate = new Date("Mar 24, 2018 09:00:00").getTime();
 
@@ -32,16 +18,12 @@ var x = setInterval(function() {
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     // Output the result in an element with id="demo"
-    document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+    document.getElementById("countDownDate").innerHTML = days + "d " + hours + "h "
     + minutes + "m " + seconds + "s ";
 
     // If the count down is over, write some text
     if (distance < 0) {
         clearInterval(x);
-        document.getElementById("demo").innerHTML = "EXPIRED";
+        document.getElementById("countDownDate").innerHTML = "EXPIRED";
     }
 }, 1000);
-</script>
-
-</body>
-</html>
